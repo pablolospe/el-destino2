18ee230a96442b2bb559f406b6ae332e16d4313d
pablolospe <yosoypxl@gmail.com>
2024-09-10 14:14:34 -0300

    th lcdth

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// en un archivo .d.ts en tu proyecto (ej. custom.d.ts)
+declare module '@astrolib/seo' {
+    export const AstroSeo: any; // Ajusta el tipo `any` al tipo real si lo conoces
+  }
+  
